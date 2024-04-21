82ccda0a5d20e84b1d9d0217eb0292eaac45743b
Akpahsamuel <sakpah@st.ug.edu.gh>
2024-04-21 15:42:24 +0000

    ui folder was added to the quiz game

---
+++ day_34/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+question_bank = []
+# for question in question_data:
+#     question_text = question["question"]
+#     question_answer = question["correct_answer"]
+#     new_question = Question(question_text, question_answer)
+#     question_bank.append(new_question)
